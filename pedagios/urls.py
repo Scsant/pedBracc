ce4105a7fcf1e15c81a39b2980b6965046f65ba8
Scsant <110061187+Scsant@users.noreply.github.com>
2025-01-03 15:36:19 -0300

    Update urls.py

--- pedagios/urls.py
+++ pedagios/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import RegistroValePedagioAPIView
+from .views import RegistroValePedagioAPIView, DownloadExcelAPIView
 
 urlpatterns = [
     path('api/vale-pedagio/', RegistroValePedagioAPIView.as_view(), name='registro_vale_pedagio'),
+    path('api/download-excel/', DownloadExcelAPIView.as_view(), name='download_excel'),
 ]
